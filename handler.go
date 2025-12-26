05411abe3d40b99877b009d36d0c88a661419ec6
Tong Zhu <40535540+TongZhu-Ellen@users.noreply.github.com>
2025-12-26 17:07:25 +0800

    v0.3: 引入 chi 路由分流，保持 POST /api/user 简单测试通过
    - 引入 chi 路由库进行 HTTP 方法分流 / Introduced chi router for HTTP method routing
    - POST /api/user 已通过 Postman 测试 / POST /api/user verified via Postman
    - 为后续实现 RUD（Read, Update, Delete）功能打基础 / Lays groundwork for implementing remaining RUD operations
    - 依然是简单、直白、可用版本 / Still a simple, straightforward, functional version

--- handler.go
+++ handler.go
@@ -9,22 +9,25 @@ import (
 func createHandler(w http.ResponseWriter, rp *http.Request) {
 	up := &User{}
 
-	err1 := json.NewDecoder(rp.Body).Decode(up)
-	if err1 != nil {
+	err := json.NewDecoder(rp.Body).Decode(up)
+	if err != nil {
 		w.WriteHeader(400)
-		w.Write([]byte("Decode error: " + err1.Error()))
+		w.Write([]byte("Decode error: " + err.Error()))
 		return 
 	} 
 
-	err2 := createService(up) 
-	if err2 != nil {
+	err = createService(up) 
+	if err != nil {
+
+
 		w.WriteHeader(500)
-		w.Write([]byte("Creation error: " + err2.Error()))
+		w.Write([]byte("Internal error: " + err.Error()))	
 		return
+
 	}
 
 	w.WriteHeader(200)
-	w.Write([]byte("User of id " + up.Id + " created."))
+	w.Write([]byte("User of id *** " + up.Id + " *** created."))
 
 
 }
--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"net/http"
 	"log"
+	"github.com/go-chi/chi"
 )
 
 func main() {
 
 	initDB()
 
-	http.HandleFunc("/api/user", createHandler) 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := chi.NewRouter()
+
+	r.Post("/api/user", createHandler)
+
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 	
 }
