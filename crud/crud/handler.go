8c864a314a585eee46669df4ee638cc1ad0802a2
Tong Zhu <40535540+TongZhu-Ellen@users.noreply.github.com>
2026-02-04 11:15:34 +0800

    v1.1.0: user.Password is hashed now
    hashed_pw, err := bcrypt.GenerateFromPassword([]byte(input_pw), bcrypt.DefaultCost)

---
+++ crud/crud/handler.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"log"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/go-chi/chi/v5"
+)
+
+func Create(w http.ResponseWriter, rp *http.Request) {
+	infp := &UserInfo{}
+
+	// 1️⃣ 解析请求
+	err := json.NewDecoder(rp.Body).Decode(infp)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+	if infp.Username == "" || infp.Password == "" {
+		http.Error(w, "bad username or password selected!", http.StatusBadRequest)
+		return 
+	}
+	up := infp.makeUser(uuid.New())
+	
+
+	err = create(up)
+	if err != nil {
+		log.Println("DB error:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+
+	// 43️⃣ 设置响应头 + 返回 JSON
+	w.Header().Set("Location", fmt.Sprintf("/user/%s", up.UserID))
+	w.WriteHeader(http.StatusCreated)  
+}
+
+func Read(w http.ResponseWriter, rp *http.Request) {
+	idStr := chi.URLParam(rp, "id")
+	userID, err := uuid.Parse(idStr)
+	if err != nil {
+		// 非法 UUID，直接返回 404
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+	
+
+	up, err := read(userID)
+	if err != nil {
+		log.Println("DB error:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	if up == nil {
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+
+	// 设置响应头 + 返回 JSON
+	up.Password = "******"
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(up)
+	if err != nil {
+		log.Println("JSON encode error:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+
+func Update(w http.ResponseWriter, rp *http.Request) {
+	
+	// get userID
+	idStr := chi.URLParam(rp, "id")
+	userID, err := uuid.Parse(idStr)
+	if err != nil {
+		// 非法 UUID，直接返回 404
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+	
+	// get other info
+	infp := &UserInfo{}
+	err = json.NewDecoder(rp.Body).Decode(infp)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+	if infp.Username == "" || infp.Password == "" {
+		http.Error(w, "bad username or password!", http.StatusBadRequest)
+		return 
+	}
+
+	up := infp.makeUser(userID)
+	
+	
+	rows, err := update(up)
+	if err != nil {
+		log.Println("DB error:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	if rows == 0 {
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+
+	
+
+	w.WriteHeader(204)
+
+}
+
+func Delete(w http.ResponseWriter, rp *http.Request) {
+	
+
+	idStr := chi.URLParam(rp, "id") 
+	userID, err := uuid.Parse(idStr)
+	if err != nil {
+		// 非法 UUID，直接返回 404
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+
+	rows, err := delete(userID)
+	if err != nil {
+		log.Println("DB error:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	if rows == 0 {
+		http.Error(w, "user not found", http.StatusNotFound)
+	}
+
+	w.WriteHeader(http.StatusNoContent) // 204
+}
---
+++ crud/crud/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+	r  *chi.Mux
+)
+
+func main() {
+	initDB()
+	initChi()
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
